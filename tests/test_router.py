da5e082ecc2426bccf0297f6493d0f1376544e55
Eric Gazoni <eric@adimian.com>
2023-03-17 18:20:40 +0100

    update tests to match desired API

--- tests/test_router.py
+++ tests/test_router.py
@@ -3,20 +3,8 @@ import dataclasses
 import pytest
 
 from kova.protocol.login_pb2 import LoginRequest
-from kova.router import Handler, Context, Publish, Reply
+from kova.router import Publish, Reply
 from kova.server import Router
-from kova.types import Message
-
-
-class PrintHandler(Handler):
-    async def __call__(
-        self,
-        context: Context,
-        message: Message,
-        reply: Reply,
-        publish: Publish,
-    ):
-        print(message)
 
 
 @dataclasses.dataclass
@@ -27,34 +15,37 @@ class Msg:
     headers: dict[str, str] | None = None
 
 
-def test_handler_can_be_defined_for_route():
+@pytest.mark.asyncio
+async def test_handler_can_be_defined_for_route():
     router = Router()
-    route = router.add_route("test.handler", LoginRequest)
-    route.add_handler(PrintHandler)
+
+    @router.route("test.handler", message_type=LoginRequest)
+    async def handle_login(message, reply: Reply):
+        if reply:
+            await reply(f"hello {message.email}".encode())
+
+    assert router.routes["test.handler"][0].__name__ == "handle_login"
 
 
 @pytest.mark.asyncio
-async def test_router_can_evaluate_messages(ctx, queue):
+async def test_router_can_evaluate_messages(queue):
+    router = Router(queue=queue)
+
     req = LoginRequest()
     req.email = "hello@acme.test"
 
-    class TestLoginHandler(Handler):
-        async def __call__(
-            self,
-            context: Context,
-            message: LoginRequest,  # type: ignore[override]
-            reply: Reply,
-            publish: Publish,
-        ):
-            if reply:
-                await reply(f"hello {message.email}".encode())
+    @router.route("test.handler", message_type=LoginRequest)
+    async def handle_login(
+        message: LoginRequest,
+        reply: Reply,
+        publish: Publish,
+    ):
+        if reply:
+            await reply(f"hello {message.email}".encode())
+        publish("security.audit", f"{message.email} just logged in".encode())
 
-    router = Router(queue=queue)
-    route = router.add_route("test.handler", LoginRequest)
-    route.add_handler(TestLoginHandler)
-
-    await route(
-        context=ctx,
+    await router.dispatch(
+        subject="test.handler",
         msg=Msg(
             data=req.SerializeToString(),
             subject="test.handler",
@@ -64,3 +55,6 @@ async def test_router_can_evaluate_messages(ctx, queue):
 
     msg = await anext(queue.messages("_INBOX.test.handler"))
     assert msg == b"hello hello@acme.test"
+
+    msg = await anext(queue.messages("security.audit"))
+    assert msg == b"hello@acme.test just logged in"
