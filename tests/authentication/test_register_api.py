b06ac4b3f2513ee725cd022b49bd7efdbb013f21
Eric Gazoni <eric@adimian.com>
2023-04-24 17:18:55 +0200

    adapt to httpx now

--- tests/authentication/test_register_api.py
+++ tests/authentication/test_register_api.py
@@ -6,9 +6,7 @@ from sqlalchemy import select
 @pytest.mark.anyio
 async def test_user_can_register_with_their_email(client, session):
     res = client.post("/register", json={"email": "alice@acme.test"})
-    assert res
-
-    # s = await session
+    assert res.status_code == 200
 
     result = await session.execute(
         select(User).where(User.email == "alice@acme.test")
