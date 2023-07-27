86c546ee6fcd79d303415fe221f22335e6ec61ad
Chama El Majeny <chama@adimian.com>
2023-07-27 15:01:29 +0200

    fixed buffer remove function

--- kova/message_buffer.py
+++ kova/message_buffer.py
@@ -38,158 +38,135 @@ class Buffer(Dependable):
         except sqlite3.Error as error:
             logger.error("Error occurred - ", error)
 
-    def save(self, message: bytes) -> str:
+    def _create_connexion(self):
+        connexion = None
+        try:
+            connexion = sqlite3.connect(self._path)
+        except sqlite3.Error as error:
+            logger.error("Error occurred - ", error)
 
+        return connexion
+
+    def save(self, message: bytes) -> str:
         if not isinstance(message, bytes):
             raise TypeError("value must be of type bytes")
 
         name = ULID()
 
-        try:
-            connexion = sqlite3.connect(self._path)
-            cursor = connexion.cursor()
-            logger.debug("Buffer DB init")
+        connexion = self._create_connexion()
+        cursor = connexion.cursor()
 
-            cursor.execute(
-                f"""
-                INSERT INTO {self.subject}
-                (name, message)
-                VALUES (?,?)""",
-                (str(name), message),
-            )
+        cursor.execute(
+            f"""
+            INSERT INTO {self.subject}
+            (name, message)
+            VALUES (?,?)""",
+            (str(name), message),
+        )
 
-            connexion.commit()
-            logger.debug("Message saved in buffer")
+        connexion.commit()
+        logger.debug("Message saved in buffer")
 
-            cursor.close()
-
-            connexion.close()
-            logger.debug("SQLite Connection closed")
-
-        except sqlite3.Error as error:
-            logger.error("Error occurred - ", error)
+        cursor.close()
+        connexion.close()
 
         return str(name)
 
     def get(self) -> bytes | None:
-        try:
-            connexion = sqlite3.connect(self._path)
-            cursor = connexion.cursor()
-            logger.debug("Buffer DB init")
+        connexion = self._create_connexion()
+        cursor = connexion.cursor()
 
-            requete = cursor.execute(
-                f"""
-                SELECT name, message FROM {self.subject} ORDER BY name ASC
-            """
-            )
+        requete = cursor.execute(
+            f"""
+            SELECT name, message FROM {self.subject} ORDER BY name ASC
+        """
+        )
 
-            name, message = requete.fetchone()
+        result = requete.fetchone()
 
+        if result is None:
+            message = None
+        else:
+            message = result[1]
             cursor.execute(
                 f"""
                 DELETE FROM {self.subject} WHERE name LIKE ?
             """,
-                (name,),
+                (result[0],),
             )
 
             connexion.commit()
-            logger.debug("Message deleted from buffer")
+            logger.debug("Message fetched from buffer")
 
-            cursor.close()
-
-            connexion.close()
-            logger.debug("SQLite Connection closed")
-
-        except sqlite3.Error as error:
-            logger.error("Error occurred - ", error)
+        cursor.close()
+        connexion.close()
 
         return message
 
     def get_all(self) -> List[bytes] | None:
-        try:
-            connexion = sqlite3.connect(self._path)
-            connexion.row_factory = lambda cursor, row: row[0]
-            cursor = connexion.cursor()
-            logger.debug("Buffer DB init")
+        connexion = self._create_connexion()
+        connexion.row_factory = lambda cursor, row: row[0]
+        cursor = connexion.cursor()
 
-            requete = cursor.execute(
-                f"""
-                SELECT message FROM {self.subject} ORDER BY name ASC
-            """
-            )
+        requete = cursor.execute(
+            f"""
+            SELECT message FROM {self.subject} ORDER BY name ASC
+        """
+        )
 
-            messages = requete.fetchall()
-            logger.debug("Message saved in buffer")
+        messages = requete.fetchall()
+        logger.debug("Message fetched from buffer")
 
-            cursor.execute(
-                f"""
-                DELETE FROM {self.subject}
-            """
-            )
+        cursor.execute(
+            f"""
+            DELETE FROM {self.subject}
+        """
+        )
 
-            cursor.close()
+        connexion.commit()
 
-            connexion.close()
-            logger.debug("SQLite Connection closed")
-
-        except sqlite3.Error as error:
-            logger.error("Error occurred - ", error)
+        cursor.close()
+        connexion.close()
 
         return messages
 
     def delete_message(self, name: str):
-        try:
-            connexion = sqlite3.connect(self._path)
-            cursor = connexion.cursor()
-            logger.debug("Buffer DB init")
+        connexion = self._create_connexion()
+        cursor = connexion.cursor()
 
-            cursor.execute(
-                f"SELECT * FROM {self.subject} WHERE name = ?", (name,)
-            )
-            data = cursor.fetchone()
-            if data is None:
-                raise ValueError(f"There is no message named {name}")
+        cursor.execute(f"SELECT * FROM {self.subject} WHERE name = ?", (name,))
+        data = cursor.fetchone()
+        if data is None:
+            raise ValueError(f"There is no message named {name}")
 
-            cursor.execute(
-                f"""
-                DELETE FROM {self.subject} WHERE name LIKE ?
-            """,
-                (name,),
-            )
+        cursor.execute(
+            f"""
+            DELETE FROM {self.subject} WHERE name LIKE ?
+        """,
+            (name,),
+        )
 
-            connexion.commit()
-            logger.debug("Message deleted from buffer")
+        connexion.commit()
+        logger.debug("Message deleted from buffer")
 
-            cursor.close()
-
-            connexion.close()
-            logger.debug("SQLite Connection closed")
-
-        except sqlite3.Error as error:
-            logger.error("Error occurred - ", error)
+        cursor.close()
+        connexion.close()
 
     def remove(self):
-        # TODO : remove oldest message
-        try:
-            connexion = sqlite3.connect(self._path)
-            cursor = connexion.cursor()
-            logger.debug("Buffer DB init")
+        connexion = self._create_connexion()
+        cursor = connexion.cursor()
 
-            cursor.execute(
-                f"""
-                DELETE FROM {self.subject}
-            """
-            )
+        cursor.execute(
+            f"""
+            DELETE FROM {self.subject} ORDER BY name DESC LIMIT 1
+        """
+        )
 
-            connexion.commit()
-            logger.debug("Message deleted from buffer")
+        connexion.commit()
+        logger.debug("Message deleted from buffer")
 
-            cursor.close()
-
-            connexion.close()
-            logger.debug("SQLite Connection closed")
-        except sqlite3.Error as error:
-            logger.error("Error occurred - ", error)
+        cursor.close()
+        connexion.close()
 
 
 Dependable.register(Buffer)
